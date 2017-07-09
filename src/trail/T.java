b85f64fcd3f9e2697aa41b97396e29dcf32e1bb3
Priya <prisinh4@in.ibm.com>
2017-07-09 22:40:47 +0530

    ch

--- src/trail/T.java
+++ src/trail/T.java
@@ -5,7 +5,7 @@ public class T {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("K");
-// commit nj
+// commit nj 
 System.out.println("K");
 	}
 
